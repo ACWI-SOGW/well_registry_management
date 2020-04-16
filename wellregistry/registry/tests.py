6ef2ffdb7a8e731e8bfa2632572b786eb337f445
Andrew Yan <ayan@usgs.gov>
2020-04-16 12:23:05 -0500

    add simple test for the hello world view

--- wellregistry/registry/tests.py
+++ wellregistry/registry/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+"""
+Tests for the registry application
 
-# Create your tests here.
+"""
+
+from django.test import RequestFactory, TestCase
+
+from .views import BasePage
+
+
+class TestBasePage(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_base_page(self):
+        req = self.factory.get('/registry')
+        resp = BasePage.as_view()(req)
+        self.assertEqual(resp.status_code, 200)
