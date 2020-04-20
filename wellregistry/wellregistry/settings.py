f450f1bb63547840d5e0f6438e9f91f370629133
Andrew Yan <ayan@usgs.gov>
2020-04-20 08:24:20 -0500

    support grabbing the secret key from an imported module

--- wellregistry/wellregistry/settings.py
+++ wellregistry/wellregistry/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+try:
+    from local_settings import SECRET_KEY
+except ImportError:
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
