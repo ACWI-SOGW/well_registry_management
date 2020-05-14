8ba732164b3b5d625be680c873b5cd3d0291c049
David Uselmann <duselmann@usgs.gov>
2020-05-14 07:59:18 -0500

    correct registry_registry table ref

--- wellregistry/registry/migrations/0004_grant_client_user.py
+++ wellregistry/registry/migrations/0004_grant_client_user.py
@@ -16,19 +16,19 @@ class Migration(migrations.Migration):
     """
     initial = False
 
-    dependencies = [('registry', '0002_initial')]
+    dependencies = [('registry', '0003_registry_table')]
 
     operations = [
         # grant CRUD to app user -- after 0001_initial, this cannot be granted until the tables is created
         migrations.RunSQL(
             sql=f"""
                 GRANT INSERT, SELECT, UPDATE, DELETE
-                ON {APP_SCHEMA_NAME}.registry
+                ON {APP_SCHEMA_NAME}.registry_registry
                 TO {APP_CLIENT_USERNAME}
             """,
             reverse_sql=f"""
                 REVOKE INSERT, SELECT, UPDATE, DELETE
-                ON {APP_SCHEMA_NAME}.registry
+                ON {APP_SCHEMA_NAME}.registry_registry
                 FROM {APP_CLIENT_USERNAME}
             """),
     ]
