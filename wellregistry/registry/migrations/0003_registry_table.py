8d2bfe531590465bc5d5f7519ec68142de9e5953
David Uselmann <duselmann@usgs.gov>
2020-05-14 07:57:49 -0500

    registry table in correct schema
    using the 'migration' database setting this table ends in the correct place. it is named registry_registry because of Django uniqueness mutation.

--- wellregistry/registry/migrations/0003_registry_table.py
+++ wellregistry/registry/migrations/0003_registry_table.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
     """
     initial = False
 
-    dependencies = [('registry', '0002_create_db_users')]
+    dependencies = [('registry', '0002_create_app_users')]
 
     operations = [
         migrations.CreateModel(
