6d0863ce5681bbf81064f1a7d3d4a9e789bc2b5d
mbucknell <mbucknell@usgs.gov>
2020-06-15 09:21:45 -0500

    Change static url to start at /registry/

--- wellregistry/wellregistry/settings.py
+++ wellregistry/wellregistry/settings.py
@@ -256,5 +256,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/registry/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "wellregistry/staticfiles")
