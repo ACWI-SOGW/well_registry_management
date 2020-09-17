d4821a63ba2b1f476528f086b868e9d6a08dd885
mbucknell <mbucknell@usgs.gov>
2020-09-17 15:15:17 -0500

    Moved the validation code to the models full_clean method so that it can be used for uploading bulk files. Tests for validation were moved and updated to work as model validation tests.

--- wellregistry/registry/tests/test_admin.py
+++ wellregistry/registry/tests/test_admin.py
@@ -12,219 +12,9 @@ from django.contrib.admin.sites import AdminSite
 from django.http import HttpRequest
 from django.test import TestCase, Client
 
-from ..admin import MonitoringLocationAdmin, MonitoringLocationAdminForm
+from ..admin import MonitoringLocationAdmin
 from ..models import AgencyLookup, MonitoringLocation
 from .fake_data import TEST_RDB, TEST_NO_WELL_DEPTH_RDB, TEST_STREAM_RDB
-
-
-class TestMonitoringLocationFormAdmin(TestCase):
-    fixtures = ['test_agencies.json', 'test_countries.json', 'test_counties.json',
-                'test_states.json', 'test_altitude_datum.json',
-                'test_horizontal_datum.json', 'test_nat_aquifer.json', 'test_units.json']
-
-    def setUp(self):
-        self.form_data = {
-            'display_flag': False,
-            'agency': 'USGS',
-            'state': '39',
-            'country': 'US',
-            'county': '171',
-            'site_no': '1234567',
-            'site_name': 'My test site',
-            'dec_lat_va': '98.456',
-            'dec_long_va': '-84.567',
-            'horizontal_datum': 'NAD27',
-            'alt_va': '2',
-            'altitude_units': '1',
-            'altitude_datum': 'NAD83',
-            'nat_aqfr': 'N100GLCIAL',
-            'site_type': 'WELL',
-            'aqfr_type': 'UNCONFINED',
-            'well_depth': '4.2',
-            'well_depth_units': '1',
-            'wl_sn_flag': False,
-            'wl_baseline_flag': False,
-            'qw_sn_flag': False,
-            'qw_baseline_flag': False,
-            'wl_well_chars': '',
-            'ql_well_chars': ''
-        }
-
-    def test_valid_when_display_flag_false(self):
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertTrue(form.is_valid())
-
-    def test_invalid_when_site_no_blank(self):
-        self.form_data['site_no'] = ''
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-        self.form_data['site_no'] = '     '
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_invalid_when_site_name_blank(self):
-        self.form_data['site_name'] = ''
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-        self.form_data['site_name'] = '     '
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_invalid_when_state_blank(self):
-        self.form_data['state'] = ''
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_invalid_when_county_blank(self):
-        self.form_data['county'] = ''
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_invalid_when_latitude_blank(self):
-        self.form_data['dec_lat_va'] = ''
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-        self.form_data['dec_lat_va'] = '     '
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_invalid_when_longitude_blank(self):
-        self.form_data['dec_long_va'] = ''
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-        self.form_data['dec_long_va'] = '     '
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_invalid_when_horizontal_datum_blank(self):
-        self.form_data['horizontal_datum'] = ''
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_invalid_when_altitude_blank(self):
-        self.form_data['alt_va'] = ''
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-        self.form_data['alt_va'] = '     '
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_invalid_when_altitude_units_blank(self):
-        self.form_data['altitude_units'] = ''
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_invalid_when_altitude_datum_blank(self):
-        self.form_data['altitude_datum'] = ''
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_invalid_when_national_aquifer_blank(self):
-        self.form_data['nat_aqfr'] = ''
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_invalid_when_site_type_blank(self):
-        self.form_data['site_type'] = ''
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_valid_when_display_flag_true_sn_flags_false(self):
-        self.form_data['display_flag'] = True
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertTrue(form.is_valid())
-
-    def test_invalid_when_site_type_well_and_aquifer_type_blank(self):
-        self.form_data['site_type'] = 'WELL'
-        self.form_data['aqfr_type'] = ''
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_invalid_depth_unit_blank(self):
-        self.form_data['well_depth'] = '5'
-        self.form_data['well_depth_units'] = ''
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_invalid_depth_unit_not_blank(self):
-        self.form_data['well_depth'] = ''
-        self.form_data['well_depth_units'] = '1'
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_valid_when_display_flag_true_and_wl_sn_flags_true(self):
-        self.form_data['display_flag'] = True
-        self.form_data['wl_sn_flag'] = True
-        self.form_data['wl_well_purpose'] = 'Other'
-        self.form_data['wl_well_type'] = 'Trend'
-        form = MonitoringLocationAdminForm(self.form_data)
-
-        self.assertTrue(form.is_valid())
-
-    def test_invalid_when_display_flag_true_and_wl_sn_flag_true(self):
-        self.form_data['display_flag'] = True
-        self.form_data['wl_sn_flag'] = True
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-        self.form_data['wl_well_purpose'] = ''
-        self.form_data['wl_well_type'] = 'Trend'
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-        self.form_data['wl_well_purpose'] = 'Other'
-        self.form_data['wl_well_type'] = ''
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_invalid_when_display_flag_true_and_wl_sn_flag_true_and_wl_baseline_true(self):
-        self.form_data['display_flag'] = True
-        self.form_data['wl_sn_flag'] = True
-        self.form_data['wl_well_type'] = 'Trend'
-        self.form_data['wl_well_purpose'] = 'Other'
-        self.form_data['wl_baseline_flag'] = True
-        self.form_data['wl_well_chars'] = ''
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_valid_when_display_flag_true_and_qw_sn_flags_true(self):
-        self.form_data['display_flag'] = True
-        self.form_data['qw_sn_flag'] = True
-        self.form_data['qw_well_purpose'] = 'Other'
-        self.form_data['qw_well_type'] = 'Trend'
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertTrue(form.is_valid())
-
-    def test_invalid_when_display_flag_true_and_qw_sn_flag_true(self):
-        self.form_data['display_flag'] = True
-        self.form_data['qw_sn_flag'] = True
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-        self.form_data['qw_well_purpose'] = ''
-        self.form_data['qw_well_type'] = 'Trend'
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-        self.form_data['qw_well_purpose'] = 'Other'
-        self.form_data['qw_well_type'] = ''
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_invalid_when_display_flag_true_and_qw_sn_flag_true_and_qw_baseline_true(self):
-        self.form_data['display_flag'] = True
-        self.form_data['qw_sn_flag'] = True
-        self.form_data['qw_well_type'] = 'Trend'
-        self.form_data['qw_well_purpose'] = 'Other'
-        self.form_data['qw_baseline_flag'] = True
-        self.form_data['qw_well_chars'] = ''
-        form = MonitoringLocationAdminForm(self.form_data)
-        self.assertFalse(form.is_valid())
 
 
 class TestMonitoringLocationAdmin(TestCase):
@@ -276,7 +66,7 @@ class TestMonitoringLocationAdmin(TestCase):
         request = HttpRequest()
         request.user = self.adwr_user
         monitoring_location = MonitoringLocation.objects.create(site_no='11111111',
-                                                     agency=AgencyLookup.objects.get(agency_cd='ADWR'))
+                                                                agency=AgencyLookup.objects.get(agency_cd='ADWR'))
         self.admin.save_model(request, monitoring_location, None, None)
 
         saved_monitoring_location = MonitoringLocation.objects.get(site_no='11111111')
@@ -288,7 +78,7 @@ class TestMonitoringLocationAdmin(TestCase):
         request = HttpRequest()
         request.user = self.superuser
         monitoring_location = MonitoringLocation.objects.create(site_no='11111111',
-                                                     agency=AgencyLookup.objects.get(agency_cd='ADWR'))
+                                                                agency=AgencyLookup.objects.get(agency_cd='ADWR'))
         self.admin.save_model(request, monitoring_location, None, None)
 
         saved_monitoring_location = MonitoringLocation.objects.get(site_no='11111111')
@@ -300,7 +90,7 @@ class TestMonitoringLocationAdmin(TestCase):
         request = HttpRequest()
         request.user = self.superuser
         monitoring_location = MonitoringLocation.objects.create(site_no='11111111',
-                                                     agency=AgencyLookup.objects.get(agency_cd='ADWR'))
+                                                                agency=AgencyLookup.objects.get(agency_cd='ADWR'))
         self.admin.save_model(request, monitoring_location, None, None)
 
         saved_monitoring_location = MonitoringLocation.objects.get(site_no='11111111')
@@ -396,6 +186,7 @@ class TestMonitoringLocationAdmin(TestCase):
         client.force_login(self.adwr_user)
         resp = client.get('/registry/admin/registry/monitoringlocation/')
         self.assertFalse(resp.context['show_fetch_from_nwis_view'])
+
 
 class TestFetchFromNwisView(TestCase):
     FETCH_URL = '/registry/admin/registry/monitoringlocation/fetch_from_nwis/'
---
+++ wellregistry/registry/tests/test_models.py
@@ -0,0 +1,213 @@
+
+from decimal import Decimal
+
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from ..models import MonitoringLocation, AgencyLookup, CountyLookup, CountryLookup, HorizontalDatumLookup, StateLookup, \
+    UnitsLookup, AltitudeDatumLookup, NatAqfrLookup
+
+class TestMonitoringLocationFullClean(TestCase):
+    fixtures = ['test_agencies.json', 'test_countries.json', 'test_counties.json',
+                'test_states.json', 'test_altitude_datum.json',
+                'test_horizontal_datum.json', 'test_nat_aquifer.json', 'test_units.json']
+
+    def setUp(self):
+        self.monitoring_location = MonitoringLocation(
+            display_flag=False,
+            agency=AgencyLookup(agency_cd='USGS'),
+            state=StateLookup(pk=39),
+            country=CountryLookup(country_cd='US'),
+            county=CountyLookup(pk=171),
+            site_no='1234567',
+            site_name='My test site',
+            dec_lat_va=Decimal('98.456'),
+            dec_long_va=Decimal('-84.567'),
+            horizontal_datum=HorizontalDatumLookup(hdatum_cd='NAD27'),
+            alt_va=Decimal('2'),
+            altitude_units=UnitsLookup(unit_id=1),
+            altitude_datum=AltitudeDatumLookup(adatum_cd='NAD83'),
+            nat_aqfr=NatAqfrLookup(nat_aqfr_cd='N100GLCIAL'),
+            site_type='WELL',
+            aqfr_type='UNCONFINED',
+            well_depth=Decimal('4.2'),
+            well_depth_units=UnitsLookup(unit_id=1),
+            wl_sn_flag=False,
+            wl_baseline_flag=False,
+            qw_sn_flag=False,
+            qw_baseline_flag=False,
+            wl_well_chars='',
+            qw_well_chars=''
+        )
+
+    def test_valid_when_display_flag_false(self):
+        try:
+            self.monitoring_location.full_clean()
+        except ValidationError as errors:
+            self.fail(f'monitoring_location should be valid. Found {errors.message_dict}')
+
+    def test_invalid_when_site_no_blank(self):
+        self.monitoring_location.site_no = ''
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+        self.monitoring_location.site_no = '         '
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+    def test_invalid_when_site_name_blank(self):
+        self.monitoring_location.site_name = ''
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+        self.monitoring_location.site_name = '         '
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+    def test_invalid_when_state_blank(self):
+        self.monitoring_location.state = None
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+    def test_invalid_when_county_blank(self):
+        self.monitoring_location.county = None
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+    def test_invalid_when_latitude_blank(self):
+        self.monitoring_location.dec_lat_va = ''
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+    def test_invalid_when_longitude_blank(self):
+        self.monitoring_location.dec_long_va = ''
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+    def test_invalid_when_horizontal_datum_blank(self):
+        self.monitoring_location.horizontal_datum = None
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+    def test_invalid_when_altitude_blank(self):
+        self.monitoring_location.alt_va = ''
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+    def test_invalid_when_altitude_units_blank(self):
+        self.monitoring_location.altitude_units = None
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+    def test_invalid_when_national_aquifer_blank(self):
+        self.monitoring_location.nat_aqfr = None
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+    def test_invalid_when_altitude_datum_blank(self):
+        self.monitoring_location.altitude_datum = None
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+    def test_invalid_when_site_type_blank(self):
+        self.monitoring_location.site_type = ''
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+    def test_valid_when_display_flag_true_sn_flags_false(self):
+        self.monitoring_location.display_flag = True
+        try:
+            self.monitoring_location.full_clean()
+        except ValidationError as errors:
+            self.fail(f'monitoring_location should be valid. Found {errors.message_dict}')
+
+    def test_invalid_when_site_type_well_and_aquifer_type_blank(self):
+        self.monitoring_location.site_type = 'WELL'
+        self.monitoring_location.aqfr_type = ''
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+    def test_invalid_depth_unit_blank(self):
+        self.monitoring_location.well_depth = '5'
+        self.monitoring_location.well_depth_units = None
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+    def test_invalid_depth_unit_not_blank(self):
+        self.monitoring_location.well_depth = ''
+        self.monitoring_location.well_depth_units = UnitsLookup(unit_id=1)
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+    def test_valid_when_display_flag_true_and_wl_sn_flags_true(self):
+        self.monitoring_location.display_flag = True
+        self.monitoring_location.wl_sn_flag = True
+        self.monitoring_location.wl_well_purpose = 'Other'
+        self.monitoring_location.wl_well_type = 'Trend'
+        try:
+            self.monitoring_location.full_clean()
+        except ValidationError as errors:
+            self.fail(f'monitoring_location should be valid. Found {errors.message_dict}')
+
+    def test_invalid_when_display_flag_true_and_wl_sn_flag_true(self):
+        self.monitoring_location.display_flag = True
+        self.monitoring_location.wl_sn_flag = True
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+        self.monitoring_location.wl_well_type = 'Trend'
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+        self.monitoring_location.wl_well_type = ''
+        self.monitoring_location.wl_well_purpose = 'Other'
+
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+    def test_invalid_when_display_flag_true_and_wl_sn_flag_true_and_wl_baseline_true(self):
+        self.monitoring_location.display_flag = True
+        self.monitoring_location.wl_sn_flag = True
+        self.monitoring_location.wl_well_purpose = 'Other'
+        self.monitoring_location.wl_well_type = 'Trend'
+        self.monitoring_location.wl_baseline_flag = True
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+
+    def test_valid_when_display_flag_true_and_qw_sn_flags_true(self):
+        self.monitoring_location.display_flag = True
+        self.monitoring_location.qw_sn_flag = True
+        self.monitoring_location.qw_well_purpose = 'Other'
+        self.monitoring_location.qw_well_type = 'Trend'
+        try:
+            self.monitoring_location.full_clean()
+        except ValidationError as errors:
+            self.fail(f'monitoring_location should be valid. Found {errors.message_dict}')
+
+
+    def test_invalid_when_display_flag_true_and_qw_sn_flag_true(self):
+        self.monitoring_location.display_flag = True
+        self.monitoring_location.qw_sn_flag = True
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+        self.monitoring_location.qw_well_type = 'Trend'
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+        self.monitoring_location.qw_well_type = ''
+        self.monitoring_location.qw_well_purpose = 'Other'
+
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
+
+
+    def test_invalid_when_display_flag_true_and_qw_sn_flag_true_and_qw_baseline_true(self):
+        self.monitoring_location.display_flag = True
+        self.monitoring_location.qw_sn_flag = True
+        self.monitoring_location.qw_well_purpose = 'Other'
+        self.monitoring_location.qw_well_type = 'Trend'
+        self.monitoring_location.qw_baseline_flag = True
+        with self.assertRaises(ValidationError):
+            self.monitoring_location.full_clean()
