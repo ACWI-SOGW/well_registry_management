3f73402a2d6affd76558e0832c2b55187c654dfd
dsiemon2 <dsiemon2@gmail.com>
2020-10-23 12:39:44 -0700

    fixed issues reported in pull request

--- wellregistry/registry/admin/fetch_from_nwis.py
+++ wellregistry/registry/admin/fetch_from_nwis.py
@@ -1,7 +1,7 @@
 """
 Custom fetch from nwis form and view for the Django admin
 """
-from wellregistry import lcl_aqfr_dict
+from wellregistry import nwis_aquifer_lookups
 import requests
 
 from django.conf import settings
@@ -45,12 +45,13 @@ class FetchFromNwisView(FormView):
         return True, 'Valid site'
 
     @staticmethod
-    def _get_lcl_aqfr_name(dictnry, aqfr_cd, state_cd):
-        for item in list(dictnry):
-            if item['Aqfr_Cd'] == aqfr_cd and item['State_Cd'] == state_cd:
-                print(item['Aqfr_Nm'] + " for " +  aqfr_cd)
-                return item['Aqfr_Nm']
-        return {'status' : "key not found"}
+    def _get_lcl_aqfr_name(list_aqr_lookups, aqfr_cd, state_cd):
+        try:
+            for item in list_aqr_lookups:
+                if item['Aqfr_Cd'] == aqfr_cd and item['State_Cd'] == state_cd:
+                    return item['Aqfr_Nm']
+        except KeyError:
+            print("key not found in dictionary")
 
     @staticmethod
     def _get_monitoring_location(self, site_data, user):
@@ -104,7 +105,7 @@ class FetchFromNwisView(FormView):
         monitoring_location.well_depth = float(site_data['well_depth_va'])
         monitoring_location.well_depth_units = UnitsLookup.objects.get(unit_id=1)
         monitoring_location.nat_aqfr = NatAqfrLookup.objects.get(nat_aqfr_cd=site_data['nat_aqfr_cd'])
-        monitoring_location.local_aquifer_name = self._get_lcl_aqfr_name(lcl_aqfr_dict, site_data['aqfr_cd'], site_data['state_cd'])
+        monitoring_location.local_aquifer_name = self._get_lcl_aqfr_name(nwis_aquifer_lookups, site_data['aqfr_cd'], site_data['state_cd'])
         monitoring_location.site_type = 'SPRING' if site_data['site_tp_cd'] == 'SP' else 'WELL'
         monitoring_location.aqfr_type = nwis_aqfr_type_cd_to_aqfr_type[site_data['aqfr_type_cd']]
         monitoring_location.update_user = user
@@ -148,7 +149,7 @@ class FetchFromNwisView(FormView):
                 else:
                     valid, message = self._validate_site(site)
                     if valid:
-                        monitoring_location = self._get_monitoring_location(self, site, self.request.user)
+                        monitoring_location = self._get_monitoring_location(self,site, self.request.user)
 
                         monitoring_location.save()
                         return redirect(reverse('admin:registry_monitoringlocation_change',
--- wellregistry/wellregistry/__init__.py
+++ wellregistry/wellregistry/__init__.py
@@ -6,12 +6,11 @@ __version__ = '0.1.0dev'
 
 import csv
 import os
-import pprint
 from django.conf import settings
 
 INITIAL_DATA_DIR = os.path.join(settings.BASE_DIR, 'registry/management/commands/initial_data/')
-lcl_aqfr_dict=[]
+nwis_aquifer_lookups=[]
 data_src = os.path.join(INITIAL_DATA_DIR, 'lcl_aqfr.csv')
 reader = csv.DictReader(open(data_src, 'r'))
 for row in reader:
-    lcl_aqfr_dict.append(row)
+     nwis_aquifer_lookups.append(row)
