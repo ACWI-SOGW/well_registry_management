f6ef9a77f72d0d7ad42bfa453a3d689181f44d0e
mbucknell <mbucknell@usgs.gov>
2020-10-27 14:12:38 -0500

    Putting code that accesses the django settings in the project directory fails when running with gunicorn as the settings have not yet been initialized. Moved the code to read the lookups to the registry application (which makes sense since that is where it is used.

---
+++ wellregistry/registry/__init__.py
@@ -0,0 +1,15 @@
+import csv
+import os
+from django.conf import settings
+
+def _get_nwis_aquifer_lookups():
+    lookups = []
+    data_src = os.path.join(settings.BASE_DIR, 'registry/management/commands/initial_data', 'lcl_aqfr.csv')
+    reader = csv.DictReader(open(data_src, 'r'))
+    for row in reader:
+        lookups.append(row)
+
+    return lookups
+
+
+nwis_aquifer_lookups = _get_nwis_aquifer_lookups()
